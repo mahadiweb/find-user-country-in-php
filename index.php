0f1e9015734bd2dc7f8c66e5b06842a4bc023209
Md.Mahadi Hasan <64768414+mahadiweb@users.noreply.github.com>
2021-12-04 18:58:18 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,13 @@
 <?php
-
-$conn = mysqli_connect("localhost","root","","database");
-
-	
-
-//$ip = $_SERVER['REMOTE_ADDR'];
-$ip = "151.135.120.0";
 // this is where you include the code that gets the country
 // you can find the code for this file on the link below    
 include("geoiploc.php");
+
+$ip = $_SERVER['REMOTE_ADDR'];
+//$ip = "151.135.120.0";
 
 // this is where you create the variable that get you the name of the country
 $country = getCountryFromIP($ip, " Name ");
 echo $country;
 
-$query = mysqli_query($conn,"INSERT INTO data(name,password) values('$country','hello');");
-if ($query) {
-	echo "yes";
-}else{
-	echo "no";
-}
 ?>
